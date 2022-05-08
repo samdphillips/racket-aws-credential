824a0bd51b34af1ba7b1be8f9194c9105a0d6aef
Sam Phillips <samdphillips@gmail.com>
2022-05-07 17:31:15 -0700

    Correct package name in info.rkt

--- aws-credential-keyring-lib/info.rkt
+++ aws-credential-keyring-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define name "aws-credential-lib")
+(define name "aws-credential-keyring-lib")
 (define collection "aws")
 (define version "0.1")
 (define deps '("base" "keyring-lib" "aws-credential-lib"))
