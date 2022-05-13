d19ede556139e563022ef5cad1ca2e1bb4858e56
Sam Phillips <samdphillips@gmail.com>
2022-05-13 14:31:58 -0700

    Add public interface for ec2-instance and file credentials

---
+++ aws-credential-lib/ext/credential/ec2-instance.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+(require "private/ec2-instance.rkt")
+(provide (all-from-out "private/ec2-instance.rkt"))
+
---
+++ aws-credential-lib/ext/credential/file.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+(require "private/file.rkt")
+(provide (all-from-out "private/file.rkt"))
+
