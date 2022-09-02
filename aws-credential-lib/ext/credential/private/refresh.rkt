7120fb58f790117ebb4ebf10def4f37d3492947f
Sam Phillips <samdphillips@gmail.com>
2022-09-01 17:57:24 -0700

    Refresh credential fixes
    - Add more checking for refresh thread termination in `aws-credential-get` to
      avoid deadlock during a get.
    - Refresh thread protocol is synchronous req/rpy so I have removed extra reply
      channel allocation as part of `aws-credential-get`

--- aws-credential-lib/ext/credential/private/refresh.rkt
+++ aws-credential-lib/ext/credential/private/refresh.rkt
@@ -2,6 +2,7 @@
 
 (require racket/contract
          racket/exn
+         racket/match
          "interface.rkt"
          "log.rkt")
 
@@ -9,26 +10,30 @@
   refresh-credential
   (contract-out
     [make-refresh-credential
-      (->* ((-> any/c any/c aws-credential?)
+      (->* ((-> any/c any/c any/c aws-credential?)
             (-> (values (list/c string? string? string?)
                         positive?)))
            (#:wiggle (>=/c 0))
            aws-credential?)]))
 
-(struct refresh-credential (pr ch)
+(struct refresh-credential (pr req-ch rpy-ch)
   #:methods gen:aws-credential
   [(define (aws-credential-get cr)
-     (when (thread-dead? (refresh-credential-pr cr))
-       (error 'aws-credential-get
-              "refresh thread has died"))
-     (define rpy-ch (make-channel))
-     (channel-put (refresh-credential-ch cr) rpy-ch)
-     (apply values (channel-get rpy-ch)))])
+     (log-aws-credential-debug "getting credential from ~a" cr)
+     (match-define (refresh-credential pr req-ch rpy-ch) cr)
+     (define oops-dead-evt
+       (handle-evt (thread-dead-evt pr)
+                   (λ (t) (error 'aws-credential-get "refresh thread has died: ~a" cr))))
+     (sync oops-dead-evt
+           (channel-put-evt req-ch #t))
+     (sync oops-dead-evt
+           (handle-evt rpy-ch (lambda (v) (apply values v)))))])
 
-(define (refresh-process refresh wiggle req-ch)
+(define (refresh-process refresh wiggle req-ch rpy-ch)
   (define (expire-evt expires)
     (alarm-evt (* 1000 expires)))
   (define (update-keys)
+    (log-aws-credential-info "refreshing credential")
     (define-values (keys expires) (refresh))
     (log-aws-credential-info "updated credential expires: ~a" expires)
     (log-aws-credential-info "updated credential expires with wiggle: ~a"
@@ -38,7 +43,7 @@
     (sync (handle-evt (expire-evt expires)
                       (λ (e) (update-keys)))
           (handle-evt req-ch
-                      (λ (rpy-ch)
+                      (λ (ignore)
                          (channel-put rpy-ch keys)
                          (run keys expires)))))
   (with-handlers ([exn:fail?
@@ -48,8 +53,9 @@
     (update-keys)))
 
 (define (make-refresh-credential maker refresh #:wiggle [wiggle 60])
-  (define ch (make-channel))
+  (define req-ch (make-channel))
+  (define rpy-ch (make-channel))
   (define pr
-    (thread (λ () (refresh-process refresh wiggle ch))))
-  (maker pr ch))
+    (thread (λ () (refresh-process refresh wiggle req-ch rpy-ch))))
+  (maker pr req-ch rpy-ch))
 
