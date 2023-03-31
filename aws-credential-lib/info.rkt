fe232e762323647046bbeb9766122f34becf17a4
Sam Phillips <samdphillips@gmail.com>
2023-03-31 16:34:10 -0700

    Change dep `http-easy` -> `http-easy-lib`

--- aws-credential-lib/info.rkt
+++ aws-credential-lib/info.rkt
@@ -3,6 +3,6 @@
 (define name "aws-credential-lib")
 (define collection "aws")
 (define version "0.1")
-(define deps '("base" "gregor-lib" "http-easy" "inifile"))
+(define deps '("base" "gregor-lib" "http-easy-lib" "inifile"))
 (define pkg-authors '(samdphillips@gmail.com))
 
